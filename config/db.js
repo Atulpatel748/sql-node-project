1418a1e9037e4d125e9ec2e0a45d5b9aac53a70b
Addiction023 <atulpatel09876@gmail.com>
2026-08-16 19:25:46 +0530

    Add MongoDB Express CRUD API

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "@Akanat_160107",
+  password: "your_password",
   database: "student_management",
 });
 
